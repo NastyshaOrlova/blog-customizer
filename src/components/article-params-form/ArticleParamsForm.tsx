1019dce81738f917c7713d65bbcc497a150ff934
Nastya Orlova <web.orlova.dev@gmail.com>
2025-05-19 16:50:56 +0300

    feat: Создаение общего состояния и состояние текущих настроек у формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,15 +1,36 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { ArticleStateType } from '../../constants/articleProps';
 import { useState } from 'react';
 import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	initialSettings: ArticleStateType;
+	onApplySettings: (newSettings: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	initialSettings,
+	onApplySettings,
+}: ArticleParamsFormProps) => {
+	const [formState, setFormState] = useState<ArticleStateType>(initialSettings);
 	const [isOpen, setIsOpen] = useState(false);
+	const handleApply = (e?: React.MouseEvent) => {
+		if (e) {
+			e.preventDefault();
+		}
+		onApplySettings(formState);
+		toggleForm();
+	};
+	const handleReset = () => {
+		setFormState(initialSettings);
+	};
 	const toggleForm = () => {
 		setIsOpen(!isOpen);
 	};
+
 	return (
 		<>
 			<ArrowButton isOpen={isOpen} onClick={toggleForm} />
@@ -17,8 +38,13 @@ export const ArticleParamsForm = () => {
 				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
+						<Button title='Применить' type='apply' onClick={handleApply} />
 					</div>
 				</form>
 			</aside>
